e5b7ec295d5b022715aa18e38712e642fa80e76c
Rémi Jacquemard <remi@jacquemard.ch>
2023-04-08 14:42:32 +0200

    Add basic note finder

--- src/notefinder.ts
+++ src/notefinder.ts
@@ -1,6 +1,12 @@
-import * as vscode from "vscode" ;
+import * as vscode from "vscode";
+import * as fsPromise from "fs/promises";
+import * as path from "path";
+import { strict } from "assert";
 
-class NoteFinder {
+
+export default class NoteFinder {
+    private readonly _fileNameRegex = new RegExp(/^.*note.*(\.md|\.txt)$/gis);
+
     private _paths: Iterable<vscode.Uri> = [];
 
     public set paths(paths: Iterable<vscode.Uri>) {
@@ -12,8 +18,40 @@ class NoteFinder {
     }
 
     public findNotesDocs() {
-        const uri = vscode.Uri.parse("~");
-        
-    }
+        // TODO: limit
 
+        const findInPath = async (filePath: vscode.Uri) => {
+            const fileList: Array<vscode.Uri> = [];
+            const fileStat = await fsPromise.lstat(filePath.fsPath);
+
+            if (fileStat.isDirectory()) {
+                // If the current path is a directory, recursively call findInPath for each subfile/directory
+                const dirFiles = await fsPromise.readdir(filePath.fsPath);
+
+                await Promise.all(dirFiles.map(async (subFilePath) => {
+                    const subPathUri = vscode.Uri.parse(`${filePath.scheme}://${filePath.path}/${subFilePath}`, true);
+                    fileList.push(...await findInPath(subPathUri));
+                }));
+            } else {
+                // If the current path is a file, check its format.
+                const filePathParts = filePath.path.split('/');
+                const fileName = filePathParts[filePathParts.length - 1];
+
+                if (this._fileNameRegex.test(fileName)) {
+                    fileList.push(filePath);
+                    console.log(`Found file at ${filePath.fsPath}`);
+                }
+            }
+
+            return fileList;
+        };
+
+        Array.from(this._paths).forEach(async path => {
+            //const files = await fsPromise.readdir(path.fsPath);
+            const files = await findInPath(path);
+            console.log("All found files: ");
+            console.log(files);
+        });
+
+    }
 }
